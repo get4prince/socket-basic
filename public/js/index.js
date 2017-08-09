8f673d0ae81aa51ac4aca71263efbaddb3701dab
Prince kumar <prince>
2017-08-10 02:00:44 +0530

    add css

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,12 @@ socket.on('newLocationMessage', function(message){
 jQuery('#message-form').on('submit',function(e){
 	e.preventDefault();
 
+	var messageTextbox = jQuery('[name=message]');
 	socket.emit('createMessage',{
 		from: 'user',
-		text: jQuery('[name=message]').val()
+		text: messageTextbox.val()
 	},function(){
-
+			messageTextbox.val('');
 	});
 })
 
@@ -45,12 +46,16 @@ locationButton.on('click',function(){
 			return alert('geolocation not suppot by ur broser');
 		}
 
+		locationButton.attr('disabled','disabled').text('Sending location.....');
+
 		navigator.geolocation.getCurrentPosition(function(position){
+			locationButton.removeAttr('disabled').text('Send location');
 				socket.emit('createLocationMessage',{
 					latitude: position.coords.latitude,
 					longitude: position.coords.longitude
 				});
 		},function(){
+			locationButton.removeAttr('disabled').text('Send location');
 			alert('unable to fetch location');
 		});
 });
